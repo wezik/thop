e90bc64f8e10dce78080a53c0e959ee699631031
Dominik <wezik@users.noreply.github.com>
2025-09-06 13:14:52 +0200

    Extend architecture tests

--- test/architecture/arch_test.go
+++ test/architecture/arch_test.go
@@ -9,6 +9,37 @@ import (
     "testing"
 )
 
+func TestInternalDoesNotImportCmd(t *testing.T) {
+    root := "../../internal/" // internal root
+    fset := token.NewFileSet()
+
+    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+
+        if info.IsDir() || !strings.HasSuffix(path, ".go") {
+            return nil
+        }
+
+        f, err := parser.ParseFile(fset, path, nil, parser.ImportsOnly)
+        if err != nil {
+            return err
+        }
+
+        for _, imp := range f.Imports {
+            if strings.Contains(imp.Path.Value, "/cmd/") {
+                t.Errorf("%s imports cmd package %s", path, imp.Path.Value)
+            }
+        }
+
+        return nil
+    })
+    if err != nil {
+        t.Fatal(err)
+    }
+}
+
 func TestPkgDoesNotImportInternal(t *testing.T) {
     root := "../../pkg/" // pkg root
     fset := token.NewFileSet()
@@ -30,6 +61,37 @@ func TestPkgDoesNotImportInternal(t *testing.T) {
         for _, imp := range f.Imports {
             if strings.Contains(imp.Path.Value, "/internal/") {
                 t.Errorf("%s imports internal package %s", path, imp.Path.Value)
+            }
+        }
+
+        return nil
+    })
+    if err != nil {
+        t.Fatal(err)
+    }
+}
+
+func TestPkgDoesNotImportCmd(t *testing.T) {
+    root := "../../pkg/" // pkg root
+    fset := token.NewFileSet()
+
+    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+
+        if info.IsDir() || !strings.HasSuffix(path, ".go") {
+            return nil
+        }
+
+        f, err := parser.ParseFile(fset, path, nil, parser.ImportsOnly)
+        if err != nil {
+            return err
+        }
+
+        for _, imp := range f.Imports {
+            if strings.Contains(imp.Path.Value, "/cmd/") {
+                t.Errorf("%s imports cmd package %s", path, imp.Path.Value)
             }
         }
 
