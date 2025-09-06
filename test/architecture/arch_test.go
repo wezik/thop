56df7314a5da2c70e3d349c2defa718be9b7734e
Dominik <wezik@users.noreply.github.com>
2025-09-06 12:21:06 +0200

    Arch test

---
+++ test/architecture/arch_test.go
@@ -0,0 +1,41 @@
+package architecture
+
+import (
+    "go/parser"
+    "go/token"
+    "os"
+    "path/filepath"
+    "strings"
+    "testing"
+)
+
+func TestPkgDoesNotImportInternal(t *testing.T) {
+    root := "../../pkg/" // pkg root
+    fset := token.NewFileSet()
+
+    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+
+        if info.IsDir() || !strings.HasSuffix(path, ".go") {
+            return nil
+        }
+
+        f, err := parser.ParseFile(fset, path, nil, parser.ImportsOnly)
+        if err != nil {
+            return err
+        }
+
+        for _, imp := range f.Imports {
+            if strings.Contains(imp.Path.Value, "/internal/") {
+                t.Errorf("%s imports internal package %s", path, imp.Path.Value)
+            }
+        }
+
+        return nil
+    })
+    if err != nil {
+        t.Fatal(err)
+    }
+}
