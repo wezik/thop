9a2795f8d5f8fc8b9ee5680c0aa9a7a6855b0998
Dominik <wezik@users.noreply.github.com>
2025-09-07 14:45:10 +0200

    Extend architecture test restrictions

--- test/architecture/arch_test.go
+++ test/architecture/arch_test.go
@@ -5,99 +5,139 @@ import (
 	"go/token"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
 	"testing"
 )
 
-func TestInternalDoesNotImportCmd(t *testing.T) {
-	root := "../../internal/" // internal root
-	fset := token.NewFileSet()
+func listImports(filePath string) []string {
+	f, err := parser.ParseFile(token.NewFileSet(), filePath, nil, parser.ImportsOnly)
+	if err != nil {
+		return nil
+	}
 
+	var result []string
+	for _, imp := range f.Imports {
+		result = append(result, imp.Path.Value)
+	}
+	return result
+}
+
+func listGoFilesInTree(root string) []string {
+	var files []string
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			return err
+			panic(err)
 		}
 
 		if info.IsDir() || !strings.HasSuffix(path, ".go") {
 			return nil
 		}
 
-		f, err := parser.ParseFile(fset, path, nil, parser.ImportsOnly)
-		if err != nil {
-			return err
-		}
-
-		for _, imp := range f.Imports {
-			if strings.Contains(imp.Path.Value, "/cmd/") {
-				t.Errorf("%s imports cmd package %s", path, imp.Path.Value)
-			}
-		}
+		files = append(files, path)
 
 		return nil
 	})
+
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
+
+	return files
 }
 
-func TestPkgDoesNotImportInternal(t *testing.T) {
-	root := "../../pkg/" // pkg root
-	fset := token.NewFileSet()
+func TestInternalDependencies(t *testing.T) {
+	// given
+	files := listGoFilesInTree("../../internal/")
+	restrictedImportPaths := []string{
+		"/cmd/",
+		"\"github.com/spf13/cobra\"",
+		"\"go.uber.org/dig\"",
+	}
 
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
+	type ImportEntry struct {
+		ImportPath string
+		FilePath   string
+	}
+
+	// when
+	var dependencies []ImportEntry
+	for _, file := range files {
+		imports := listImports(file)
+		for _, imp := range imports {
+			dependencies = append(dependencies, ImportEntry{
+				ImportPath: imp,
+				FilePath:   file,
+			})
 		}
+	}
 
-		if info.IsDir() || !strings.HasSuffix(path, ".go") {
-			return nil
-		}
-
-		f, err := parser.ParseFile(fset, path, nil, parser.ImportsOnly)
-		if err != nil {
-			return err
-		}
-
-		for _, imp := range f.Imports {
-			if strings.Contains(imp.Path.Value, "/internal/") {
-				t.Errorf("%s imports internal package %s", path, imp.Path.Value)
+	// then
+	t.Run("no restricted dependencies", func(t *testing.T) {
+		for _, dep := range dependencies {
+			if slices.Contains(restrictedImportPaths, dep.ImportPath) {
+				t.Errorf("%s depends on %s", dep.FilePath, dep.ImportPath)
 			}
 		}
-
-		return nil
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
-}
 
-func TestPkgDoesNotImportCmd(t *testing.T) {
-	root := "../../pkg/" // pkg root
-	fset := token.NewFileSet()
+	t.Run("no external logger dependencies", func(t *testing.T) {
+		allowedPath := "../../internal/log/"
+		for _, dep := range dependencies {
+			// skip logger implementations
+			if strings.Contains(dep.FilePath, allowedPath) {
+				continue
+			}
 
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() || !strings.HasSuffix(path, ".go") {
-			return nil
-		}
-
-		f, err := parser.ParseFile(fset, path, nil, parser.ImportsOnly)
-		if err != nil {
-			return err
-		}
-
-		for _, imp := range f.Imports {
-			if strings.Contains(imp.Path.Value, "/cmd/") {
-				t.Errorf("%s imports cmd package %s", path, imp.Path.Value)
+			if strings.Contains(dep.ImportPath, "log") && dep.ImportPath != "\"thop/pkg/log\"" {
+				t.Errorf("%s depends on external logger implementation %s", dep.FilePath, dep.ImportPath)
 			}
 		}
-
-		return nil
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
 }
+
+func TestPkgDependencies(t *testing.T) {
+	// given
+	files := listGoFilesInTree("../../pkg/")
+	restrictedImportPaths := []string{
+		"/cmd/",
+		"/internal/",
+		"\"github.com/spf13/cobra\"",
+		"\"go.uber.org/dig\"",
+	}
+	type ImportEntry struct {
+		ImportPath string
+		FilePath   string
+	}
+
+	// when
+	var dependencies []ImportEntry
+	for _, file := range files {
+		imports := listImports(file)
+		for _, imp := range imports {
+			dependencies = append(dependencies, ImportEntry{
+				ImportPath: imp,
+				FilePath:   file,
+			})
+		}
+	}
+
+	// then
+	t.Run("no restricted dependencies", func(t *testing.T) {
+		for _, dep := range dependencies {
+			if slices.Contains(restrictedImportPaths, dep.ImportPath) {
+				t.Errorf("%s depends on %s", dep.FilePath, dep.ImportPath)
+			}
+		}
+	})
+
+	t.Run("no external logger dependencies", func(t *testing.T) {
+		for _, dep := range dependencies {
+			if strings.Contains(dep.ImportPath, "log") && dep.ImportPath != "\"thop/pkg/log\"" {
+				t.Errorf("%s depends on external logger implementation %s", dep.FilePath, dep.ImportPath)
+			}
+		}
+	})
+}
+
+
